57fe211d6551106dda3019de079669dc5d2e03fe
zhengbin_Mac <1746788394@qq.com>
2016-03-05 22:18:34 +0800

    wd.bookstore-0.1 搭建基本框架，SSH&SSM框架实现，前端imgAreaSelect图片编辑，ajaxfileupload图片异步上传。

---
+++ src/com/bookstore/entity/Book.java
@@ -0,0 +1,53 @@
+package com.bookstore.entity;
+
+public class Book {
+	private int id;
+	private String cover;
+	private String name;
+	private String author;
+	private String isbn;
+	private int number;
+	private double price;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getCover() {
+		return cover;
+	}
+	public void setCover(String cover) {
+		this.cover = cover;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getIsbn() {
+		return isbn;
+	}
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+	public int getNumber() {
+		return number;
+	}
+	public void setNumber(int number) {
+		this.number = number;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+}
